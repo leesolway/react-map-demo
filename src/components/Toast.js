21157287b17ae3c56b618c3c555878e02ad463c7
Lee Solway <lee@digitalanvil.co.uk>
2020-01-31 02:59:40 +0000

    Style updates to Toast - incl colour and fonts

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -8,13 +8,15 @@ const ToastWrapper = styled.div`
   flex-direction: column;
 
   border-radius: 8px;
-  padding: 8px 16px;
+  padding: 0px 8px;
   overflow: auto;
-  background: #505050;
+  background: rgba(51, 51, 51, 0.9);
   margin: 0px;
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1), 0 1px 8px 0 rgba(0, 0, 0, 0.1);
   font-style: normal;
-  color: #b7b7b6;
+  font-size: 16px;
+  color: #ffffff;
+  height: 40px;
 
   position: absolute;
   top: 32px;
